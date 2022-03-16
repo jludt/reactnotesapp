c1675d98f883f065229b6370afdd32d030d13659
jludt <68578085+jludt@users.noreply.github.com>
2022-03-15 22:07:07 -0400

    adding css and new notes capability

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Note from './Note/Note';
-
+import NewNoteForm from './NewNoteForm/NewNoteForm';
 
 class App extends Component {
 
   constructor(props){
     super(props);
+    this.addNote = this.addNote.bind(this);
 
     this.state ={
       notes: [
-        {id: 1, noteContent: "Note 1 here!54321 "},
-        {id: 2, noteContent: "Note 2 here!!!!"},
-        {id: 3, noteContent: "Note 3 here yo!"},
-        {id: 3, noteContent: "Note 4 test!"}
+        {id: 1, noteContent: "Note 1 here!"},
+        {id: 2, noteContent: "Note 2 here!"},
+
       ],
 
     }
+  }
+
+  addNote(note){
+    //push the note onthe the notes array
+    const previousNote = this.state.notes;
+    previousNote.push({id: previousNote.length + 1, noteContent: note});
+    this.setState({
+      notes: previousNote
+    })
+
   }
   render() {
     return (
@@ -34,8 +44,10 @@ class App extends Component {
                 <Note noteContent={note.noteContent} notedId = {note.id} key={note.id}/>
               )
             })
-            
           }
+      </div>
+      <div className='notesFooter'>
+          <NewNoteForm addNote={this.addNote}/>
       </div>
       </div>
     );
---
+++ src/NewNoteForm/NewNoteForm.jsx
@@ -0,0 +1,45 @@
+import React, { Component} from 'react';
+import './NewNoteForm.css';
+
+class NewNoteForm extends Component{
+    constructor(props){
+        super(props);
+            this.state = {
+                newNoteContent: '',
+            };
+            this.handleUserInput = this.handleUserInput.bind(this);
+            this.writeNote = this.writeNote.bind(this);
+        }
+
+        //When the user input changes, set the newNoteContent
+        //to the new note value
+        handleUserInput(e){
+            this.setState({
+                newNoteContent: e.target.value, // value of the text input
+            })
+        }
+
+        writeNote(){
+            // call method that sets teh noteContent for a note to
+            // the avlue of the input
+            this.props.addNote(this.state.newNoteContent);
+            this.setState({
+                newNoteContent: '',
+            })
+        }
+
+        render(){
+            return(
+             <div className='formWrapper'>
+                 <input className='noteInput'
+                 placeholder='Write a new note...'
+                 value = {this.state.newNoteContent}
+                 onChange={this.handleUserInput} />
+                 <button className='noteButton'
+                 onClick={this.writeNote}>Add Note</button>
+             </div>   
+            )
+        }
+}
+
+export default NewNoteForm;
